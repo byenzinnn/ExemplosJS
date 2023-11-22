e1b42684bc19295bce72c14348ca94524d6940d2
byenzinnn <enzodearaujogrellmann@hotmail.com>
2023-11-22 20:58:00 -0300

    Só umas coisas a mais

--- Aula 03/assets/js/strings.js
+++ Aula 03/assets/js/strings.js
@@ -1,11 +1,22 @@
 const renderiza_string = function(elemento){
-    console.log(elemento.value);
-    console.log(elemento.value.trim());
-    console.log(elemento.value.valueOf());
+    const city = 'Brasil';
+    const valor = elemento.value;
+    const outro_valor = valor.concat(" ...");
+    console.log(valor);
+    console.log(valor.trim());
+    console.log(valor.valueOf());
 
     const render = document.getElementById('render_string');
-    render.innerText = `
-    <p>${valor.toUpperCase()}<p>
-    <p>${valor.toLowerCase()}<p>
-    `
+    render.innerHTML = `
+    <p>${valor.toUpperCase()}</p>
+    <p>${valor.toLowerCase()}</p>
+    <p>${valor.substring(0, 4)}</p>
+    <p>Começa com script? ${valor.startsWith("Script")}<p>
+    <p>Termina com legal? ${valor.endsWith("legal")}<p>
+    <p>Existe a palavra chuva? ${valor.trim().toLowerCase
+    ().includes("chuva")}</p>
+    <p>${outro_valor}</p>
+    <p>${valor.replace("a", 5)}</p>
+    <p>${valor.replaceAll("a", 9)}</p>
+    `;
 }
