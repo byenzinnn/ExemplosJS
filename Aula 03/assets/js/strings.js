9da08410811d1b76a518fcca49f35945a25143bd
byenzinnn <enzodearaujogrellmann@hotmail.com>
2023-11-29 18:54:37 -0300

    Mais items

--- Aula 03/assets/js/strings.js
+++ Aula 03/assets/js/strings.js
@@ -1,10 +1,17 @@
 const renderiza_string = function(elemento){
     const city = 'Brasil';
     const valor = elemento.value;
-    const outro_valor = valor.concat(" ...");
+    const outro_valor = valor.concat("...");
+    const vetor_da_string = valor.split('@');
+    const string_do_vetor = vetor_da_string.join('-');
+    const ultimos2Digitos = vetor_da_string[0].substring(0, 2);
+    vetor_da_string[0] = ultimos2Digitos.padEnd(vetor_da_string[0].lenght, '*');
+    const maskedNumber = vetor_da_string.join('@');
+
     console.log(valor);
     console.log(valor.trim());
-    console.log(valor.valueOf());
+    console.log(vetor_da_string);
+    console.log(maskedNumber);
 
     const render = document.getElementById('render_string');
     render.innerHTML = `
@@ -16,7 +23,11 @@ const renderiza_string = function(elemento){
     <p>Existe a palavra chuva? ${valor.trim().toLowerCase
     ().includes("chuva")}</p>
     <p>${outro_valor}</p>
-    <p>${valor.replace("a", 5)}</p>
-    <p>${valor.replaceAll("a", 9)}</p>
+    <p>${valor.replace("[email]", "enzo@local.br")}</p>
+    <p>${valor.replaceAll("[telefone]", '9988-4455')}</p>
+    <p>${valor.repeat(0)}</p>
+    <p> A texto <b>${valor}</b> possui ${valor.length}
+    caracteres</p>
+
     `;
 }
