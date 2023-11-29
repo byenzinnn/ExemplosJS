e9786e8d88f034c42031de4eca43187aba025028
byenzinnn <enzodearaujogrellmann@hotmail.com>
2023-11-29 19:31:33 -0300

    Mais itens

---
+++ Aula 03/assets/js/numbers.js
@@ -0,0 +1,14 @@
+const pegaInput = function(elemento){
+    const render_number = document.getElementById('render_number');
+    let valor = new Number(elemento.value);
+
+    
+    render_number.innerHTML = 
+    `${typeof valor} <br>
+    ${Number.parseInt(valor)} <br>
+    ${Number.parseFloat(valor)} <br>
+    ${valor.toPrecision(2)} <br>
+    ${valor.toFixed(2)}
+    `;
+
+};
